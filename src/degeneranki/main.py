248f6a6b664cf8233fb787fd28eefe46f1e90e82
Nam Tran <me@omn0mn0m.com>
2024-01-21 02:48:24 -0500

    Added inventory display

--- src/degeneranki/main.py
+++ src/degeneranki/main.py
@@ -1,9 +1,11 @@
 import functools
-import urllib3
 import sys
+import urllib3
 from os import path
+from enum import Enum
 
 from .gacha import GachaMachine
+from .database import Database
 
 # import the main window object (mw) from aqt
 from aqt import gui_hooks, mw
@@ -11,6 +13,8 @@ from aqt import gui_hooks, mw
 from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
+
+from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
 
 gacha = GachaMachine()
 http = urllib3.PoolManager()
@@ -21,6 +25,79 @@ MAX_ROLL_IMAGE_HEIGHT = 720
 sys.path.insert(1, path.abspath(path.dirname(__file__)))
 root_project_dir = path.abspath(path.dirname(__file__))
 res_files_dir = path.join(root_project_dir, "res")
+
+class DegenerankiWidget(QTabWidget):
+    
+    def __init__(self, *args, **kwargs):
+        super(DegenerankiWidget, self).__init__(*args, **kwargs)
+
+        self.gachaTab = GachaWidget()
+        self.addTab(self.gachaTab, "Gacha")
+
+        self.charactersTab = InventoryWidget(InventoryWidget.InventoryTypes.CHARACTERS)
+        self.addTab(self.charactersTab, "Characters")
+
+        self.weaponsTab = InventoryWidget(InventoryWidget.InventoryTypes.WEAPONS)
+        self.addTab(self.weaponsTab, "Weapons")
+
+class InventoryWidget(QScrollArea):
+
+    class InventoryTypes(Enum):
+        WEAPONS = 0,
+        CHARACTERS = 1
+    
+    def __init__(self, inventory_type, *args, **kwargs):
+        super(InventoryWidget, self).__init__(*args, **kwargs)
+        
+        self.network_manager = QNetworkAccessManager()
+        self.network_manager.finished.connect(self.on_finished)
+
+        self.wrapper_widget = QWidget()
+        self.grid_layout = QGridLayout(self.wrapper_widget)
+        self.setWidget(self.wrapper_widget)
+        self.setWidgetResizable(True)
+
+        self.max_col_items = 4
+
+        self.row = 0
+        self.col = 0
+
+        self.grid_info = {}
+
+        if inventory_type == InventoryWidget.InventoryTypes.WEAPONS:
+            for i, data in enumerate(gacha.data.get_owned_weapons()):
+                info = gacha.data.weapons[data[0]]
+                self.add_to_grid(info)
+        elif inventory_type == InventoryWidget.InventoryTypes.CHARACTERS:
+            for i, data in enumerate(gacha.data.get_owned_characters()):
+                info = gacha.data.characters[data[0]]
+                self.add_to_grid(info)
+
+    def add_to_grid(self, info):
+        request = QNetworkRequest(QUrl(info.icon))
+        self.network_manager.get(request)
+
+    @pyqtSlot(QNetworkReply)
+    def on_finished(self, reply):
+        inventory_label = QLabel()
+        label_measure = self.width() // self.max_col_items
+        inventory_label.setFixedWidth(label_measure)
+        inventory_label.setFixedHeight(label_measure)
+        
+        image = QImage()
+        image.loadFromData(reply.readAll())
+        
+        image_pixmap = QPixmap.fromImage(image)
+        inventory_label.setPixmap(image_pixmap.scaled(inventory_label.size(), 
+                                                      Qt.AspectRatioMode.KeepAspectRatio, 
+                                                      Qt.TransformationMode.SmoothTransformation))
+        self.grid_layout.addWidget(inventory_label, self.row, self.col)
+
+        self.col += 1
+
+        if self.col == self.max_col_items:
+            self.col = 0
+            self.row += 1
 
 class GachaWidget(QWidget):
     
@@ -46,18 +123,20 @@ class GachaWidget(QWidget):
         self.roll_image.setPixmap(self.wish_bg_pixmap)
         
         self.layout.addWidget(self.roll_image, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.button_layout = QHBoxLayout()
+        self.layout.addLayout(self.button_layout)
         
         self.roll_button = QPushButton("Roll (Cost: 100 Gacha Points)", self)
         self.roll_button.clicked.connect(self.roll)
         self.layout.addWidget(self.roll_button)
-
-        self.gacha_points = QLabel("Gacha Points Left: {}".format(gacha.data.gacha_points))
-        self.layout.addWidget(self.gacha_points)
-        
-        self.roll_stats_layout = QHBoxLayout()
         
         # Bottom bar
+        self.roll_stats_layout = QHBoxLayout()
         self.layout.addLayout(self.roll_stats_layout)
+
+        self.gacha_points = QLabel("Gacha Points Left: {}".format(gacha.data.gacha_points))
+        self.roll_stats_layout.addWidget(self.gacha_points)
 
         self.lifetime_rolls = QLabel("Lifetime Rolls: {}".format(gacha.data.lifetime_rolls))
         self.roll_stats_layout.addWidget(self.lifetime_rolls)
@@ -81,7 +160,7 @@ class GachaWidget(QWidget):
         image = QImage()
 
         if hasattr(roll, 'gacha'):
-            image.loadFromData(http.request("GET", roll.icon).data)
+            image.loadFromData(http.request("GET", roll.gacha).data)
         else:
             image.loadFromData(http.request("GET", roll.icon).data)
         image_pixmap = QPixmap(image)
@@ -116,6 +195,9 @@ class GachaWidget(QWidget):
         self.pity_4_star.setText("4-Star Pity: {}".format(gacha.data.pity_4_star))
         self.pity_5_star.setText("5-Star Pity: {}".format(gacha.data.pity_5_star))
 
+    def show_inventory(self):
+        pass
+
     def show_white(self, roll_pixmap):
         self.roll_image.setPixmap(self.flash_bg_pixmap)
         self.timer.stop()
@@ -130,7 +212,7 @@ class GachaWidget(QWidget):
         self.timer.stop()
 
 def showWidget() -> None:
-    mw.myWidget = widget = GachaWidget()
+    mw.myWidget = widget = DegenerankiWidget()
     widget.resize(1280, 900)
     widget.show()
 
